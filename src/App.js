03975d8ac73e2be34a9e5549d7f31ecc67099041
Vastro-lorde <omatsolaseund@gmail.com>
2022-11-19 08:36:07 +0100

    setting up

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './assets/pds-logo.svg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <img src={logo} alt='pds logo'/>
+        </div>
+        <h1>Pixo Dynamic Space Store</h1>
       </header>
     </div>
   );
